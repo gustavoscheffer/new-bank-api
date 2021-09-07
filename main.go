4c2a4e7bc3e74c4b5a1b0f8eb1748a494c30e450
Gustavo Scheffer <gustavo.scheffer@outlook.com>
2021-09-07 18:30:31 -0300

    Configure logs

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"new-bank-api/router"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
 )
 
 func init() {
@@ -15,6 +17,12 @@ func init() {
 func main() {
 
 	app := fiber.New()
+
+	app.Use(requestid.New())
+
+	app.Use(logger.New(logger.Config{
+		Format: "[${ip}]:${port} - ${pid} ${locals:requestid} ${status} - ${method} ${path}​\n​",
+	}))
 
 	router.SetupRoutes(app)
 
